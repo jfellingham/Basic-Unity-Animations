741c46c7aeab19b5c66b05e555d064313c7a92be
Josh <josh.fellingham@gmail.com>
2019-07-29 16:59:41 +1200

    First Commit
    Add basic Unity project

---
+++ Test 3/Assets/button2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class button2 : MonoBehaviour
+{
+    public Animator anima;
+    public Button b;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = b.GetComponent<Button>();
+      
+        btn.onClick.AddListener(play);
+     
+
+        // a = Resources.Load<Animation>("Take 001");
+        //anim = gameObject.GetComponent<Animation>();
+        // anim["Take 001"].layer = 0; 
+        //  a.Play();
+        anima = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void play()
+    {
+
+        anima.Play("Take 002");
+        Debug.Log("Button 2");
+    }
+}
---
+++ Test 3/Assets/testAnimation.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class testAnimation : MonoBehaviour
+{
+
+ 
+    public Animator anima;
+    public Button b;
+    public Button c; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        // b.onClick.AddListener(play);
+        Button btn = b.GetComponent<Button>();
+        btn.onClick.AddListener(play);
+
+        Button btn1 = c.GetComponent<Button>();
+        btn1.onClick.AddListener(play2);
+        // a = Resources.Load<Animation>("Take 001");
+        //anim = gameObject.GetComponent<Animation>();
+        // anim["Take 001"].layer = 0; 
+        //  a.Play();
+        anima = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void play()
+    {
+        anima.Play("Take 001");
+        Debug.Log("Button 1"); 
+    }
+    void play2() {
+        anima.Play("Take 002");
+        Debug.Log("Button 2");
+    }
+}
